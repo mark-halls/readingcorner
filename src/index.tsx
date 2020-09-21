b312d8335a9b475220ac22132c5350f1ebd13654
Mark Halls <mark.halls@gmail.com>
2020-09-20 19:09:21 -0700

    Fix regenerator runtime error

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
+// import JSZip from "jszip";
 
 if (module.hot) {
   module.hot.accept();
 }
+
+// const unzip = new JSZip();
+const unzipFile = async () => {
+  //   const unzipped = await unzip.loadAsync("/static/books/pg16328.epup");
+  const test = await new Promise(() => {});
+  //   console.log(unzipped);?
+};
+
+unzipFile();
 
 console.log("Hello from tsx!");
 
